921d6c1164ec2f7db519b60d680485b5da96e5f4
carlosandf <carlos.andres.ff2912@gmail.com>
2023-07-22 19:13:48 -0500

    Init course

---
+++ clase-1/01.os-info.js
@@ -0,0 +1,12 @@
+const { log } = require('node:console')
+const os = require('node:os')
+
+log('Información del sistema operativo:')
+log('----------------------------------')
+log('Nombre del sistema operativo:', os.platform())
+log('Version del sistema operativo:', os.version())
+log('Arquitectura:', os.arch())
+log('CPUs:', os.cpus())
+log('Memoria libre:', os.freemem())
+log('Memoria total:', os.totalmem())
+log('Uptime:', os.uptime())
