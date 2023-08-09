950a0c926f1c93ec7712a860f25887ccd7c95ad4
carlosandf <carlos.andres.ff2912@gmail.com>
2023-08-08 19:26:02 -0500

    Add new enpoints

--- clase-3/index.js
+++ clase-3/index.js
@@ -1,13 +1,61 @@
 const express = require('express');
+const crypto = require('node:crypto');
+const movies = require('./movies.json');
+const { validateMovie } = require('./validations/movieSchema.js');
 
 const app = express();
 app.disable('x-powered-by');
+app.use(express.json());
 
 const PORT = process.env.PORT ?? 12345;
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hola Mundo' });
+app.use(express.static('public'));
+
+// obtener todas las peliculas o filtrar por genero
+app.get('/movies', (req, res) => {
+  const { genre } = req.query;
+
+  if (genre) {
+    const filteredMovies = movies.filter(
+      movie => movie.genre.some(g => g.toLowerCase() === genre.toLowerCase())
+    );
+    return res.json(filteredMovies);
+  }
+  res.status(200).json(movies);
 });
+
+// GET ONE BY ID
+app.get('/movies/:id', (req, res) => {
+  const { id } = req.params;
+  const movie = movies.find(movie => movie.id === id);
+
+  if (movie) return res.json(movie);
+
+  res.status(404).json({ message: 'Movie not found' });
+});
+
+app.post('/movies', async (req, res) => {
+  const result = await validateMovie(req.body);
+
+  if (result.error) {
+    return res.status(400).json({ error: JSON.parse(result.error.message) });
+  }
+
+  const newMovie = {
+    id: crypto.randomUUID(),
+    ...result.data
+  };
+
+  /*
+    Esto NO es REST, porque estamos guardando
+    el estado de la aplicación en memoria
+  */
+  movies.push(newMovie);
+
+  res.status(201).json(newMovie);
+});
+
+// TODO: Acualizar una pelicula -> Pending
 
 app.listen(PORT, () => {
   if (process.env.ENV === 'PROD') {
